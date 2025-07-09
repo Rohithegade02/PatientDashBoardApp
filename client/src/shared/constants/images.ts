238c066407ffd24ff71a766cad4a9c61aea0408b
rohit-techenhance” && git config --global user.email rohit.hegade@techenhance.com“ <rohithegade8@gmail.com>
2025-07-09 13:22:29 +0530

    Refactor image constants to include type annotations for better TypeScript support

--- client/src/shared/constants/images.ts
+++ client/src/shared/constants/images.ts
@@ -1,9 +1,17 @@
+import { ImageSourcePropType } from 'react-native'
+
 export const IMAGES = {
-    DOCTOR_FIRST: require('../../../assets/images/doctor-1.png'),
-    DOCTOR_SECOND: require('../../../assets/images/doctor-2.png'),
-    DOCTOR_THIRD: require('../../../assets/images/doctor-3.png'),
-    DRUGS: require('../../../assets/images/drugs.png'),
-    MEDICAL_RECORD: require('../../../assets/images/medical-team.png'),
-    HOSPITAL: require('../../../assets/images/hospital.png'),
-    AMBULANCE: require('../../../assets/images/ambulance.png'),
+    DOCTOR_FIRST:
+        require('../../../assets/images/doctor-1.png') as ImageSourcePropType,
+    DOCTOR_SECOND:
+        require('../../../assets/images/doctor-2.png') as ImageSourcePropType,
+    DOCTOR_THIRD:
+        require('../../../assets/images/doctor-3.png') as ImageSourcePropType,
+    DRUGS: require('../../../assets/images/drugs.png') as ImageSourcePropType,
+    MEDICAL_RECORD:
+        require('../../../assets/images/medical-team.png') as ImageSourcePropType,
+    HOSPITAL:
+        require('../../../assets/images/hospital.png') as ImageSourcePropType,
+    AMBULANCE:
+        require('../../../assets/images/ambulance.png') as ImageSourcePropType,
 } as const
